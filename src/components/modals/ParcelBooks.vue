4a90a89f008ef1e345514de2e9733add114c9b83
Nepein Andrey <nepein96@gmail.com>
2020-03-21 22:34:52 +0300

    global search

--- src/components/modals/ParcelBooks.vue
+++ src/components/modals/ParcelBooks.vue
@@ -1,25 +1,26 @@
 <template>
-    <sui-modal :open="opened" @clickAwayModal="closeCallback" @displayChanged="closeCallback">
-        <sui-modal-header class="modal-header">{{ title }}</sui-modal-header>
-        <sui-modal-content class="modal-content">
-            <slot name="content"></slot>
-        </sui-modal-content>
-    </sui-modal>
+  <sui-modal :open="opened" @clickAwayModal="closeCallback" @displayChanged="closeCallback">
+    <sui-modal-header class="modal-header">{{ title }}</sui-modal-header>
+    <sui-modal-content class="modal-content" scrolling>
+      <slot name="content"></slot>
+    </sui-modal-content>
+  </sui-modal>
 </template>
 
 <script>
 export default {
-    props: {
-        title: String,
-        opened: Boolean,
-        closeCallback: Function,
-    },
-    computed: {
-        isOpen() {
-            return this.opened;
-        },
-    },
+  props: {
+    title: String,
+    opened: Boolean,
+    closeCallback: Function
+  },
+  computed: {
+    isOpen() {
+      return this.opened;
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
